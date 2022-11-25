7e4660816514614629430a482b66981f3bad90c0
nightyknite <nightyknite3@gmail.com>
2022-11-26 02:47:58 +0900

    候補文字決定

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const puppeteer = require('puppeteer');
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
 const { candiateWords } = require('./candiateWords');
+
+let wordleResponse = [];
+
 const delay = (time) => {
     return new Promise((resolve) => { 
         setTimeout(resolve, time)
     });
 }
-const getResultWords = async (page) => {
+
+const getWordleResponse = async (page) => {
     const rowSelector = 'div[class*="Row-module_row"]';
     const datas = await page.evaluate((selector) => {
       let datas = [];
@@ -42,14 +46,14 @@ const convCell = (state) => {
     if (state === 'empty') return '';
 }
 
-const generateResultCells =  (list) => {
-    for (row of list) {
-        console.log(row.map((c) => convCell(c.state)).join(''));
+const generateResultCells =  () => {
+    for (row of wordleResponse) {
+      console.log(row.map((c) => convCell(c.state)).join(''));
     }
 };
 
-const getCorrectWord =  (list) => {
-    for (row of list) {
+const getCorrectWord =  () => {
+    for (row of wordleResponse) {
         const isCorrect = row.every((c) => {return c.state === 'correct';});
         if (isCorrect) return row.map((c) => c.text).join('');
     }
@@ -57,61 +61,117 @@ const getCorrectWord =  (list) => {
 };
 
 
-const getCandidateWord = (result) => {
+const getCandidateWord = () => {
+    
+    let words = [];
+    words = candiateWords;
 
-    return candiateWords[Math.floor(Math.random() * candiateWords.length)];
+    // 全て存在する文字のみの候補
+    const presentLetters = wordleResponse.flat().filter(v => (v.state === 'present' || v.state === 'correct')).map(v => v.text);
+    if (presentLetters && presentLetters.length > 0) {
+        words = words.filter((word) => {            
+            return presentLetters.every(v => {
+                return (word.indexOf(v) >= 0);
+            });
+        });
+    }
+
+    // 存在しない文字を除外した候補
+    let absentLetters = wordleResponse.flat().filter(v => v.state === 'absent').map(v => v.text);
+    if (absentLetters && absentLetters.length > 0) {
+        if (presentLetters && presentLetters.length > 0) {
+            // presentにある文字は除外する
+            absentLetters = absentLetters.filter(v => {
+                return !presentLetters.some(w => w === v);
+            });
+        }
+        words = words.filter((word) => {
+            return !absentLetters.some(v => {
+                return (word.indexOf(v) >= 0)
+            });
+        });
+    }
+
+    // 前回使用した文字を除外
+    words = words.filter((word) => {
+        for (items of wordleResponse) {
+            if (word === (items[0].text + items[1].text + items[2].text + items[3].text + items[4].text)) {
+                return false;
+            }
+        }
+        return true;
+    });
+
+    // 存在してかつ位置も同じ条件で絞り込む
+    let correctWords = ['', '', '', '', ''];
+    for (const items of wordleResponse) {
+        for (let i = 0; i < 5; i++) {
+            if (items[i].state === 'correct') {
+                correctWords[i] = items[i].text;
+            }
+        }
+    }
+    for (let i = 0; i < correctWords.length; i++) {
+        if (correctWords[i] !== '') {
+            words = words.filter((word) => { 
+                return word[i] === correctWords[i];
+            });
+        }
+    }
+
+    console.log("candiate", words);
+    const candidateWord = words[Math.floor(Math.random() * words.length)];
+    return candidateWord;
 }
 
 const searchCorrectWords = async (page) => {
     let correctWord = '';
-    let result = [];
     let candiateWord = '';
+
     for (let word of ['rugby', 'moved', 'plans', 'witch']) {
         candiateWord = word;
         console.log(candiateWord);
         await inputWords(page, [candiateWord]);
         await delay(1000);
-        result = await getResultWords(page);
+        wordleResponse = await getWordleResponse(page);
         await delay(1000);
-        generateResultCells(result);
+        generateResultCells();
         // 4回以内で正解
-        correctWord = getCorrectWord(result);
+        correctWord = getCorrectWord();
         if (correctWord !== '') {
             return;
         }
     }
 
-
     // 5回目入力
-    candiateWord = getCandidateWord(result);
+    candiateWord = getCandidateWord();
     console.log(candiateWord);
     await inputWords(page, [candiateWord]);
     await delay(1000);
-    result = await getResultWords(page);
+    wordleResponse = await getWordleResponse(page);
     await delay(1000);
-    generateResultCells(result);
+    generateResultCells();
     // 5回目で正解
-    correctWord = getCorrectWord(result);
+    correctWord = getCorrectWord();
     if (correctWord !== '') {
-        console.log(correctWord);
+        console.log('answer', correctWord);
         return;
     }
 
     // 6回目入力
-    candiateWord = getCandidateWord(result);
+    candiateWord = getCandidateWord();
     console.log(candiateWord);
     await inputWords(page, [candiateWord]);
     await delay(1000);
-    result = await getResultWords(page);
+    wordleResponse = await getWordleResponse(page);
     await delay(1000);
-    generateResultCells(result);
+    generateResultCells();
     // 6回目で正解
-    correctWord = getCorrectWord(result);
+    correctWord = getCorrectWord();
     if (correctWord !== '') {
-        console.log(correctWord);
+        console.log('answer', correctWord);
         return;
     }
-    console.log(result);
     return;
 
 };
